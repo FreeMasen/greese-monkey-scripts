2dbb739574b073fd36a2515fcc6914fb9d4d5577
Robert Masen <r.f.masen@gmail.com>
2023-05-12 10:34:28 -0500

    octoprint est end time

--- let selector = "strong[data-bind*=printT.js
+++ let selector = "strong[data-bind*=printT.js
@@ -4,6 +4,25 @@ function again() {
 }
 
 function try_update_end_time() {
+    let existing = document.querySelector("#end-time");
+    if (!existing) {
+        let pre = document.querySelector(".progress");
+        if (!pre) {
+            console.log("no progress element found");
+            return again();
+        }
+        let title = document.createElement("span");
+        title.innerText = "Est. End Time";
+
+        pre.parentElement.insertBefore(title, pre);
+        let colon = document.createTextNode(" : ");
+        pre.parentElement.insertBefore(colon, pre);
+        existing = document.createElement("strong");
+        existing.setAttribute("id", "end-time");
+        pre.parentElement.insertBefore(existing, pre);
+        let br = document.createElement("br");
+        pre.parentElement.insertBefore(br, pre)
+    }
     let timeLeftEle = document.querySelector(selector);
     if (!timeLeftEle) return again();
     let parts = timeLeftEle.innerText.split(":");
@@ -24,12 +43,7 @@ function try_update_end_time() {
         hh -= 12
     }
     let end_time = `${hh}:${mm} ${am_pm}`;
-    let container = document.getElementById("sidebar");
-    if (!container) {
-        console.log(end_time);
-        return again()
-    };
-    container.setAttribute("title", end_time);
+    existing.innerText = end_time;
     again();
 }
 
